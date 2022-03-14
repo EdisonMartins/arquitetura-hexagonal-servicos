b438997e5c452222c0916c63df31c9962f90f25d
Edison Martins <martinsegmj@gmail.com>
2022-03-13 21:19:09 -0300

    Classe para meu filho Arthur mexer!

---
+++ src/main/java/conta/servicos/respositorio/Arthur.java
@@ -0,0 +1,11 @@
+package conta.servicos.respositorio;
+
+/**
+ * Classe criada para o meu filho Arthur mexer, já que ele está aqui e não me deixa estudar.
+ * Pelo menos, desta vez, ele não desligou o computador.
+ * Não foi por falta de tentativas, mas agora, eu estava mais atento.
+ * Te amo meu filho!
+ * **/
+public class Arthur {
+    ///....,,m m
+}
